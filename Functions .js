a371c8e969cbcde1b5a39b91426a084a80ef9870
assumpter kimeu <assumpterkimeu16@gmail.com>
2018-03-08 12:16:57 +0300

    added functions

---
+++ Functions .js
@@ -0,0 +1,36 @@
+// This is a void function
+function add(){
+	var numberOne = 10;
+	var numberTwo = 20;
+	var sum = numberOne + numberTwo;
+	console.log (sum)
+}
+// call the function
+ add()
+
+function multiply(){
+	var numberOne = 10;
+	var numberTwo = 20;
+	var numberThree = 25;
+	var answer = numberOne * numberTwo * numberThree;
+
+	return answer
+
+}
+var result = multiply()
+console.log(result)
+
+function divide(){
+	var numberOne = 20;
+	var numberTwo = 10 ;
+	var answer = numberOne / numberTwo;
+	return answer
+}
+ var result = divide()
+ console.log(result)
+
+
+
+
+
+
