e040d698189099464f7033bedf44f5ab03223f7e
assumpter kimeu <assumpterkimeu16@gmail.com>
2018-03-10 12:14:02 +0300

    functions and arrays

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+var student = ["Jane","Lauren", "Ivy", "Mary","Judith","Fatma","Irine"] 
+var shoeSize = [6,8,5,7,9,12,14]
+// get the length of array
+// console.log(student.length)
+
+for(i=0;i<student.length;i+=1)
+	console.log(student[i])
---
+++ FunctionParameters.js
@@ -0,0 +1,19 @@
+function areaOfTriangile(height,base){
+	
+	var answer = 0.5 * height * base;
+	return answer
+}
+
+var results = areaOfTriangile(10, 40);
+
+var x = areaOfTriangile(50, 10);
+
+var y= areaOfTriangile(30, 20);console.log("Triangle Three : " + y)
+
+print(results)
+print(x)
+print(y)
+
+function print(answer){
+console.log("Area of Triangle:" + answer)
+}
